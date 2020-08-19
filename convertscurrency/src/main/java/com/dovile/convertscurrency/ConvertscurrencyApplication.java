a467eacfad7e803610638cfee14bacd7d083e6ef
Doreina <barkauskaite.dovile@gmail.com>
2020-08-19 20:22:57 +0300

    Create project with dependencies: Spring: web, Data JPA, DevTools and Thymeleaf, Mysql

---
+++ convertscurrency/src/main/java/com/dovile/convertscurrency/ConvertscurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.dovile.convertscurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertscurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvertscurrencyApplication.class, args);
+	}
+
+}
