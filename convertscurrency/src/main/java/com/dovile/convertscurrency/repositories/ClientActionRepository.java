d1e75b7e1042ef6b772f520a34f6716fa2cf3935
Doreina <barkauskaite.dovile@gmail.com>
2020-08-22 16:16:37 +0300

    add commit

--- convertscurrency/src/main/java/com/dovile/convertscurrency/repositories/ClientActionRepository.java
+++ convertscurrency/src/main/java/com/dovile/convertscurrency/repositories/ClientActionRepository.java
@@ -4,6 +4,9 @@ import com.dovile.convertscurrency.entities.ClientAction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * @author barkauskaite.dovile@gmail.com
+ */
 @Repository
 public interface ClientActionRepository extends JpaRepository<ClientAction, Integer> {
 }
