b726d143e87f134eba9633aca1aa8305790e2a7c
Doreina <barkauskaite.dovile@gmail.com>
2020-08-24 19:43:56 +0300

    add one more test for Ltbank class

--- convertscurrency/src/test/java/com/dovile/convertscurrency/http/LtBankTest.java
+++ convertscurrency/src/test/java/com/dovile/convertscurrency/http/LtBankTest.java
@@ -2,6 +2,11 @@ package com.dovile.convertscurrency.http;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,5 +27,18 @@ public class LtBankTest {
             assertNotNull(result);
             assertEquals("1", result.get("EUR"));
         }
+    }
+
+    @Test
+    public void testGetUrl() throws IOException {
+        String url = "https://www.lb.lt/webservices/FxRates/FxRates.asmx/getCurrentFxRates?tp=EU";
+        LtBank ltBank = new LtBank();
+        InputStream result = ltBank.getUrl(url);
+        HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+        con.addRequestProperty("User-Agent", "Mozilla/4.76");
+        int responseCode = con.getResponseCode();
+        int expResult = 200;
+        assertEquals(expResult, responseCode);
+        assertNotNull(result);
     }
 }
