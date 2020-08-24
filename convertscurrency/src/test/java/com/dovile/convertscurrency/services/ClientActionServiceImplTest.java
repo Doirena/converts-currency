1e4b75d51d81e623af7b799f44597974cadfb399
Doreina <barkauskaite.dovile@gmail.com>
2020-08-24 16:08:32 +0300

    correct test method

--- convertscurrency/src/test/java/com/dovile/convertscurrency/services/ClientActionServiceImplTest.java
+++ convertscurrency/src/test/java/com/dovile/convertscurrency/services/ClientActionServiceImplTest.java
@@ -6,11 +6,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-
 import org.mockito.junit.MockitoJUnitRunner;
-
 import java.math.BigDecimal;
 
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 /**
@@ -21,18 +20,15 @@ public class ClientActionServiceImplTest {
 
     @InjectMocks
     private ClientActionServiceImpl clientActionService;
-
     @Mock
     private ClientActionRepository clientActionRepository;
 
     @Test
     public void saveClientAction() {
         ClientAction clientAction = new ClientAction(null, "From EUR to USD change 10 GET 11.933000");
-////        given(clientActionRepository.save(clientAction)).willAnswer(invocation -> invocation.getArgument(1));
-//        doCallRealMethod().when(clientActionRepository).save(anyClientAction());
-         clientActionService.saveClientAction("EUR", "USD", "10",new BigDecimal("11.933000"));
-
+        given(clientActionRepository.save(clientAction)).willAnswer(invocation -> invocation.getArgument(0));
+//        doCallRealMethod().when(clientActionRepository).save(clientAction);
+        clientActionService.saveClientAction("EUR", "USD", "10",new BigDecimal("11.933000"));
         verify(clientActionRepository, times(1)).save(clientAction);
-
     }
 }
