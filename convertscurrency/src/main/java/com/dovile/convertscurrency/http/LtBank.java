54bc478575694342f7a782b82188f9b3dcf59f15
Doreina <barkauskaite.dovile@gmail.com>
2020-08-24 19:43:36 +0300

    correct method make flexible

--- convertscurrency/src/main/java/com/dovile/convertscurrency/http/LtBank.java
+++ convertscurrency/src/main/java/com/dovile/convertscurrency/http/LtBank.java
@@ -19,29 +19,43 @@ import java.util.logging.Logger;
  */
 public class LtBank extends CurrentFxRates {
 
+    private final static Logger logger = Logger.getLogger(LtBank.class.getName());
+
     private final String url = "https://www.lb.lt/webservices/FxRates/FxRates.asmx/getCurrentFxRates?tp=";
     private final String tp = "EU";
     private final String rate = "FxRate";
     private final String tagNameC = "Ccy";
     private final String tagNameA = "Amt";
 
-    private final static Logger logger = Logger.getLogger(LtBank.class.getName());
-
     /**
      *
+     * @param url
+     * @return return inputStream
+     */
+    InputStream getUrl(String url) {
+        URL obj = null;
+        try {
+            obj = new URL(url + tp);
+            HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+            connection.addRequestProperty("User-Agent", "Mozilla/4.76");
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Accept", "application/xml");
+            InputStream is = connection.getInputStream();
+            return is;
+        } catch (Exception e) {
+            logger.severe("Error with path!" + e.getMessage());
+            return null;
+        }
+    }
+
+    /**
      * @return data from xml file, where will be type and rate
      */
     public Map<String, String> getData() {
         Map<String, String> map = new HashMap<>();
         try {
             logger.info("Connetion for the parse data");
-            URL obj = new URL(url + tp);
-            HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
-            connection.addRequestProperty("User-Agent", "Mozilla/4.76");
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("Accept", "application/xml");
-            InputStream is = connection.getInputStream();
-
+            InputStream is = getUrl(url);
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder db = dbf.newDocumentBuilder();
             Document doc = db.parse(is);
